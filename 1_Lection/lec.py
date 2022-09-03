f8ed5e20cc51f99d0cca05027e975b241e5a6162
Igor <igor.paluhovicz@gmail.com>
2022-09-03 12:29:03 +0200

    lec_done

---
+++ 1_Lection/lec.py
@@ -0,0 +1,30 @@
+# value = None
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# print(value)
+
+# s = 'qwe'
+# print(s)
+
+# print('{} - {} - {}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+# list = ['1','2','hello', 1,2,3,4, False]
+# print(list)
+
+# print('Enter a: ')
+# a = input()
+# print('Enter b: ')
+# b = input()
+
+# print(f'{a} -- {b}')
+
+# a = 1.2345323343
+# b = 3
+# c = round(a*b, 5)
+# print(c)
+
+
+
