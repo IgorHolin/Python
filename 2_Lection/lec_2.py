1c3c29a72bf01268d2fe126950000d63509e36cf
Igor <igor.paluhovicz@gmail.com>
2022-09-10 11:23:06 +0200

    lec_2

---
+++ 2_Lection/lec_2.py
@@ -0,0 +1,29 @@
+# colors = ['green','red','blue']
+# data = open('file.txt', 'a')
+# data.writelines(colors)
+# data.close()
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# def new_string(symbol, count):
+#     return symbol * count
+# print(new_string('!', 5))    
+
+
+# def new_string(symbol, count = 3):
+#     return symbol * count
+# print(new_string('!'))
+
+
+# def concatination(*params):
+#     res: str = ""
+#     for items in params:
+#         res += items
+#     return res
+# print(concatination('a','b','c','d'))
+
+
